ca0f8e5e7c960ecf663508890801f4a34138e4b9
pikuch <55052790+pikuch@users.noreply.github.com>
2021-06-24 13:11:09 +0200

    Added the main game loop and a data loading mockup

--- HangmanGame.cs
+++ HangmanGame.cs
@@ -1,18 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hangman
 {
-    class HangmanGame
+    public class HangmanGame
     {
+        private const int MaxLives = 5;
+
+        private Random random = new Random();
+
+        private List<(string, string)> CountriesAndCities;
+
+        private string CurrentCity;
+        private string CurrentCountry;
+        private List<bool> CityMask;
+        private int Lives;
 
         public HangmanGame()
         {
+            CountriesAndCities = LoadCountriesAndCities();
+        }
 
+        private List<(string, string)> LoadCountriesAndCities()
+        {
+            return new List<(string, string)> { ("Poland", "Warsaw") };
+            // TODO: actually load data from the file
         }
 
         public void Play()
         {
+            if (CountriesAndCities.Count == 0)
+            {
+                Console.WriteLine("Sorry, there are no city names to guess.");
+                return;
+            }
 
+            while (true)
+            {
+                ResetTheGame();
+                while (Lives > 0)
+                {
+                    // TODO: the actual gameplay
+                    Lives--;
+                }
+                // TODO: play again?
+                break;
+            }
+        }
+
+        private void ResetTheGame()
+        {
+            int cityIndex = random.Next(CountriesAndCities.Count);
+            (CurrentCountry, CurrentCity) = CountriesAndCities[cityIndex];
+
+            Lives = MaxLives;
+            CityMask = Enumerable.Repeat(true, CurrentCity.Length).ToList();
         }
     }
 }
