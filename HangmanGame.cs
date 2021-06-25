9c0dd92aa54cd08f2f998bd46dac06eba50c63a4
pikuch <55052790+pikuch@users.noreply.github.com>
2021-06-25 11:30:38 +0200

    Added basic display

--- HangmanGame.cs
+++ HangmanGame.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Hangman
 {
     public class HangmanGame
     {
         private const int MaxLives = 5;
+        private const int ShowHintWhenBelowThisManyLives = 2;
+        private const char MaskCharacter = '_';
+        private const char LivesCharacter = '♥';
 
         private Random random = new Random();
 
@@ -15,6 +19,7 @@ namespace Hangman
         private string CurrentCity;
         private string CurrentCountry;
         private List<bool> CityMask;
+        private List<char> NotInWordList;
         private int Lives;
 
         private List<(string, string)> LoadCountriesAndCities()
@@ -37,12 +42,72 @@ namespace Hangman
                 ResetTheGame();
                 while (Lives > 0)
                 {
+                    Display();
                     // TODO: the actual gameplay
                     Lives--;
                 }
                 // TODO: play again?
                 break;
             }
+        }
+
+        private void Display()
+        {
+            Console.Clear();
+            DisplayMaskedCity();
+            DisplayHealth();
+            DisplayNotInWordList();
+            DisplayHint();
+        }
+
+        private void DisplayHint()
+        {
+            if (Lives < ShowHintWhenBelowThisManyLives)
+            {
+                Console.WriteLine($"Hint: it's the capital of {CurrentCountry}");
+            } else
+            {
+                Console.WriteLine();
+            }
+        }
+
+        private void DisplayNotInWordList()
+        {
+            StringBuilder notInWordListBuilder = new StringBuilder("Letters not in the word:");
+            for (int i=0; i<NotInWordList.Count; i++)
+            {
+                notInWordListBuilder.Append($" {NotInWordList[i]}");
+            }
+            Console.WriteLine(notInWordListBuilder.ToString());
+        }
+
+        private void DisplayHealth()
+        {
+            StringBuilder livesStringBuilder = new StringBuilder("Lives:");
+            for (int i=0; i<Lives; i++)
+            {
+                livesStringBuilder.Append($" {LivesCharacter}");
+            }
+            Console.WriteLine(livesStringBuilder.ToString());
+        }
+
+        private void DisplayMaskedCity()
+        {
+            Console.WriteLine("Guess the capital city:");
+            StringBuilder hiddenCityName = new StringBuilder(CurrentCity.Length * 2);
+            for (int i=0; i<CurrentCity.Length; i++)
+            {
+                if (CityMask[i])
+                {
+                    hiddenCityName.Append(MaskCharacter);
+                }
+                else
+                {
+                    hiddenCityName.Append(CurrentCity[i]);
+                }
+                hiddenCityName.Append(" ");
+            }
+            Console.WriteLine(hiddenCityName.ToString());
         }
 
         private void ResetTheGame()
@@ -52,6 +117,7 @@ namespace Hangman
 
             Lives = MaxLives;
             CityMask = Enumerable.Repeat(true, CurrentCity.Length).ToList();
+            NotInWordList = new List<char>();
         }
     }
 }
